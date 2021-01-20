59dcf2afde7f39b31da3f094f332f8c16ad91fa4
Adnan-Bacic <adnan-bacic@hotmail.com>
2021-01-20 19:26:57 +0100

    Initial commit

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,168 @@
+<template>
+  <form v-on:submit.prevent="handleSubmit">
+    <label>Email:</label>
+    <input type="email" v-model="email" required>
+
+    <label>Password:</label>
+    <input type="password" v-model="password" required>
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <label>Skills (press alt + comma to add):</label>
+    <input type="text" v-model="tempSkill" v-on:keyup.alt="addSkill">
+    <div v-for="skill in skills" v-bind:key="skill" class="pill">
+      <span v-on:click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>Accept terms and conditions</label>
+    </div>
+
+    <!--multiple checkboxes require value-->
+    <div>
+        <input type="checkbox" value="name1" v-model="names">
+        <label>name1</label>
+    </div>
+    <div>
+        <input type="checkbox" value="name2" v-model="names">
+        <label>name2</label>
+    </div>
+    <div>
+        <input type="checkbox" value="name3" v-model="names">
+        <label>name3</label>
+    </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
+    </div>
+    
+    <p>email: {{email}}</p>
+    <p>password: {{password}}</p>
+    <p>role: {{role}}</p>
+    <p>terms: {{terms}}</p>
+    <p>names: {{names}}</p>
+  </form>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email: '',
+            password: '',
+            role: '',
+            terms: false,
+            names: [],
+            tempSkill: '',
+            skills: [],
+            passwordError: ''
+        }
+    },
+    methods:{
+        addSkill(e){
+            console.log(e)
+
+            if(e.key === ',' && this.tempSkill){
+              if(!this.skills.includes(this.tempSkill)){
+                this.skills.push(this.tempSkill)
+              }
+              this.tempSkill = ''
+            }
+        },
+        deleteSkill(skill){
+          this.skills = this.skills.filter((item) => {
+            return skill !== item
+          })
+        },
+        handleSubmit(e){
+          console.log(e)
+
+          //password validation
+          if(this.password.length < 6){
+            this.passwordError = 'Password must be at least 6 characters long'
+          } else {
+            this.passwordError = ''
+          }
+
+          if(!this.passwordError){
+            console.log('email: ', this.email)
+            console.log('password: ', this.password)
+            console.log('role: ', this.role)
+            console.log('skills: ', this.skills)
+            console.log('terms accepted: ', this.terms)
+          }
+        }
+    }
+}
+</script>
+
+<style>
+  form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input, select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+  .pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+  }
+  button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+  }
+  .submit {
+    text-align: center;
+  }
+  .error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+  }
+</style>
